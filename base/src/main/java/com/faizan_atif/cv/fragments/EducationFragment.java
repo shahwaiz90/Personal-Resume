88a1425fcfe469fda21ac2c9320f1d6baa208bcd
faizan387 <faizanbutt833@gmail.com>
2019-10-14 22:32:23 +0500

    strings and density buckets

--- base/src/main/java/com/faizan_atif/cv/fragments/EducationFragment.java
+++ base/src/main/java/com/faizan_atif/cv/fragments/EducationFragment.java
@@ -42,38 +42,32 @@ public class EducationFragment extends Fragment {
         RecyclerView recyclerViewDemo = view.findViewById(R.id.recyclerViewDemo1);
         recyclerViewDemo.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        RecyclerView recyclerViewDemo2 = view.findViewById(R.id.recyclerViewDemo2);
-        recyclerViewDemo2.setLayoutManager(new LinearLayoutManager(getContext()));
+//        RecyclerView recyclerViewDemo2 = view.findViewById(R.id.recyclerViewDemo2);
+//        recyclerViewDemo2.setLayoutManager(new LinearLayoutManager(getContext()));
 
         recyclerViewDemo.setAdapter(new EducationAdapter(feedItems(), getContext()));
-        recyclerViewDemo2.setAdapter(new EducationAdapter(feedItems2(), getContext()));
+//        recyclerViewDemo2.setAdapter(new EducationAdapter(feedItems2(), getContext()));
 
         return view;
     }
     private List<EducationModel> feedItems() {
 //        String Titles = "Education Section" ;
         List<EducationModel> demoItems = new ArrayList<>();
-        demoItems.add(new EducationModel("BS","FAST", "2017 -", R.drawable.fastlogo));
-        demoItems.add(new EducationModel("FSC","PGC", "2015 - 2017", R.drawable.ieee));
-        demoItems.add(new EducationModel("Matric","St.Antony", "-2013", R.drawable.pucitlogo));
+        demoItems.add(new EducationModel("Bachelor of Computer Science(BSCS)","National University of Computer and Emerging Sciences", "(2017 - )", R.drawable.fastlogo));
+        demoItems.add(new EducationModel("F.Sc. Pre-Engineering","Punjab Group of Colleges", "(2015 - 2017)", R.drawable.pgc_logo));
+        demoItems.add(new EducationModel("Matriculation","St. Anthony High School", "(- 2015)", R.drawable.school_logo));
 
 //        DemoItem demoItem = new DemoItem(Titles);
 //        demoItems.add(demoItem);
         return demoItems;
 
     }
-    private List<EducationModel> feedItems2() {
-//        String Titles = "Education Section" ;
-        List<EducationModel> demoItems = new ArrayList<>();
-        demoItems.add(new EducationModel("IEEE","EE dept", " - 2019", R.drawable.linkedin));
-        demoItems.add(new EducationModel("Softec","Global", " - 2020", R.drawable.github));
-  //      demoItems.add(new EducationModel("Matric","St.Antony", "-2013", R.drawable.pucitlogo));
-
-//        DemoItem demoItem = new DemoItem(Titles);
-//        demoItems.add(demoItem);
-        return demoItems;
-
-    }
+//    private List<EducationModel> feedItems2() {
+//        List<EducationModel> demoItems = new ArrayList<>();
+//        demoItems.add(new EducationModel("IEEE","EE dept", " - 2019", R.drawable.linkedin));
+//        demoItems.add(new EducationModel("Softec","Global", " - 2020", R.drawable.github));
+//        return demoItems;
+//    }
 }
 
 
